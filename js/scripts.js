b44dc9ed10cc076752a62a7d1e8b6c3abc67dfd8
narimanadam <narimanadam7@gmail.com>
2017-08-03 02:29:46 +0400

    Fixing navbar fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-    // var percentage;
 
+    // Skills Percentage
     $(window).scroll(function () {
+        var percentage;
         $('.skill__percentage').each(function (index, value) {
             percentage = $(value).data('percentage');
 
@@ -11,19 +12,53 @@ $(document).ready(function () {
                     width: percentage + '%'
                 })
             }
+        });
 
-            if (scrollFromTop > 700) {
-                $('.navbar').addClass('navbar-fixed-top');
+    });
+
+    // Smooth Scrolling
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
             }
+        });
 
-        })
-
-    })
-
+    // Map Overlay
     $('.map-wrapper').find('.overlay').click(function () {
         $(this).addClass('hide');
-    })
+    });
 
+    // Isotope and Images Loaded
     $('.grid').imagesLoaded(function () {
         // images have loaded
         // init Isotope
@@ -47,6 +82,7 @@ $(document).ready(function () {
 });
 
 $(window).load(function () {
+    // Isotope and Images Loaded
     $('.grid').imagesLoaded(function () {
         // images have loaded
         // init Isotope
@@ -65,40 +101,4 @@ $(window).load(function () {
         });
     });
 
-
-    $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
 })
