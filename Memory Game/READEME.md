ec877b841f9f0660294512eb9e7b7ab6d922b9a2
Nariman Adam <n.adam@GMGADMINs-MacBook-Pro.local>
2018-04-10 15:45:18 +0400

    Finalzing Memory Game Project

---
+++ Memory Game/READEME.md
@@ -0,0 +1,16 @@
+#Memory Game Project
+
+##Table of Contents
+
+* [Overview] (#Overview)
+* [Preview] (#Preview)
+
+## Overview
+In this game you have to match two cards and each card click is counted as a move. The rating of the player is based on moves, So If
+- Moves is equal to 24 or more the rating will be 2 stars
+- Moves is equal to 36 or more the rating will be 1 star only
+
+Player will win when all the cards are matching
+
+## Preview
+https://codepen.io/nariman_adam/full/aYQYvP/
--- Memory Game/js/scripts.js
+++ Memory Game/js/scripts.js
@@ -1,63 +1,82 @@
 $(document).ready(function () {
 
     // Randomly Generate Cards Function
-    function randomGenerateCards (){
-        $(".cards").html($(".cards .card").sort(function(){
-            return Math.random()-0.5;
+    function randomGenerateCards() {
+        $(".cards").html($(".cards .card").sort(function () {
+            return Math.random() - 0.5;
         }));
     }
-
-    // Reset Game Function
-    function reset() {
-        randomGenerateCards();
-        moves = 0;
-        $('.moves').text(moves + ' Move');
-        clickCard();
-        $('.card').removeClass('flipped');
-        $('.card').removeClass('matched');
-    }
-
-    //  Randomly generate cards on page load
-    randomGenerateCards();
 
     var moves = 0;
     $('.moves').text(moves + ' Move');
 
     // On card click flip it
-    var clickedItem = '' ;
+    var clickedItem = '';
+    // Reset Game Function
+    function reset() {
+        randomGenerateCards();
+        moves = 0;
+        clickedItem = '';
+        $('.moves').text(moves + ' Move');
+        clickCard();
+        $('.card').removeClass('flipped');
+        $('.card').removeClass('matched');
+        clearInterval(myTimer);
+        $('#minutes').text('00');
+        $('#seconds').text('00');
+        if($('.fa-star').hasClass('far')){
+            $('.fa-star').removeClass('far').addClass('fas')
+        }
+        $(document).one("click", ".card", function(){
+            timer();
+        });
+    }
 
-    function clickCard () {
-        $('.card').click(function (){
+    //  Randomly generate cards on page load
+    randomGenerateCards();
+
+    // Trigger Timer on the first click of the card only
+    $(document).one("click", ".card", function(){
+        timer();
+    });
+
+    // Click Card Function
+    function clickCard() {
+        $('.card').click(function () {
             $(this).addClass('flipped');
             var clickedCard = $(this);
             moves = moves + 1;
-            if(moves <= 1) {
+            if (moves <= 1) {
                 $('.moves').text(moves + ' Move');
             } else {
                 $('.moves').text(moves + ' Moves');
             }
+
+            if( moves == 24) {
+                $('.star-3').removeClass('fas').addClass('far')
+            } else if (moves == 36) {
+                $('.star-2').removeClass('fas').addClass('far')
+            }
             // if item clicked change item cliked to true
-            if( clickedItem == '') {
-                 clickedItem = $(this);
+            if (clickedItem == '') {
+                clickedItem = $(this);
             } else {
-                if( clickedItem.data('card') == $(this).data('card')) {
+                if (clickedItem.data('card') == $(this).data('card')) {
                     $(this).addClass('matched');
-                    clickedItem.addClass('matched');  
+                    clickedItem.addClass('matched');
                     // if all matched Show Congratulations overlay  
-                    if($('.matched').length == 16) {
+                    if ($('.matched').length == 16) {
                         $('.success').addClass('show');
-                        // hide congratulations overlay after 7 sec
-                        setTimeout(function (){
-                            $('.success').removeClass('show');
-                            reset()
-                        }, 7000)
+                        stopTimer();
+                        $('.success.show').append('<p id="game-time"> You Won the Game in ' + $('#minutes').text() + ':' + $('#seconds').text() +'</p>');
+        
                     }
-                } else {                    
-                  var currentCard=$(this);
-                  var previousCard=clickedItem;
-                  currentCard.addClass('not-matched');
-                  previousCard.addClass('not-matched');
-                    setTimeout(function(){
+                } else {
+                    var currentCard = $(this);
+                    var previousCard = clickedItem;
+                    currentCard.addClass('not-matched');
+                    previousCard.addClass('not-matched');
+                    setTimeout(function () {
                         currentCard.removeClass('flipped').removeClass('not-matched');
                         previousCard.removeClass('flipped').removeClass('not-matched');
                     }, 1000);
@@ -67,11 +86,48 @@ $(document).ready(function () {
 
         });
     }
+    
 
     clickCard();
 
     // on refresh button click reset Game
-    $('#btn-refresh').click(function (){
+    $('#btn-refresh').click(function () {
         reset();
     });
+
+    // Play Again
+    $('#play-again').click(function () {
+        reset();
+        $('.success').removeClass('show');
+    })
+
+    // Timer
+    var myTimer;
+    function timer() {
+        var minutesLabel = document.getElementById("minutes");
+        var secondsLabel = document.getElementById("seconds");
+        var totalSeconds = 0;
+        myTimer = setInterval(setTime, 1000);
+
+        function setTime() {
+            ++totalSeconds;
+            secondsLabel.innerHTML = pad(totalSeconds % 60);
+            minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+        }
+
+        function pad(val) {
+            var valString = val + "";
+            if (valString.length < 2) {
+                return "0" + valString;
+            } else {
+                return valString;
+            }
+        }
+    }
+
+    function stopTimer() {
+        clearInterval(myTimer);
+    }
+
+
 });
